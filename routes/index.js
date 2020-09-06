81039fe2e7883c88197d36536164f46499268c3a
masayado <masayado@uc.cl>
2020-09-06 18:47:14 -0300

    inicialice y configuré entorno del proyecto

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var userController = require('../controllers/users');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  var result = await userController.user_get(req, res, next);
+  res.render('users', { result });
+});
+
+/* Post user  */
+router.post('/', function(req, res, next) { 
+  userController.user_create(req, res, next);
+});
+
+module.exports = router;
