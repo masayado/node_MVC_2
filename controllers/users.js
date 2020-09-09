fd940a8e7318e65a1ac419ad831ca005fa2dca16
masayado <masayado@uc.cl>
2020-09-09 13:05:14 -0300

    comente linea de cambio de ñ por nn

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ exports.user_create = function(req, res, next) {
 exports.user_get = async function(req, res, next) {
     var users = await User.find({}).sort({name:1}); //nombres en orden alfabetico
     var usersName = users.map((user)=> {
-      user.lastname = user.lastname.replace('ñ', 'nn');
+      user.lastname = user.lastname.replace('ñ', 'nn'); //cambio de ñ por nn 
       return user;
     });
     return usersName.sort();
